bbc0672f703a377cd8aebc8c421600aae21ee7ba
alessiorossotti <alessiorossotti@gmail.com>
2017-05-20 20:56:31 +0200

    iOS project refactor, added cloud handler

--- it.polimi.mobilecodegenerator/bin/ios/BookShelfTest/BookShelfTest/AppDelegate.swift
+++ it.polimi.mobilecodegenerator/bin/ios/BookShelfTest/BookShelfTest/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import CoreData
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -9,6 +10,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // Configure Firebase
+        FirebaseApp.configure()
         return true
     }
 
